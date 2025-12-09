99be5bf4ee52409fea78c4f45286623eab78e130
devcontify <developer.crystalcodes@gmail.com>
2025-12-09 18:29:35 +0100

    🧹 Clean: Remove .history directory from repository

--- .history/next.config_20250314172020.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Deaktiviere ESLint während des Builds für Vercel
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
